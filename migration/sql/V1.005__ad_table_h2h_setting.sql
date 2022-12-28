13366d6a2fc9f78ba2b22fd270192e4326b724f2
Adinandra Dharmasurya <adinandra_dharmasurya@telkomsel.co.id>
2022-12-28 21:39:16 +0700

    Add table h2h setting for provider and fee

---
+++ migration/sql/V1.005__ad_table_h2h_setting.sql
@@ -0,0 +1,29 @@
+create table h2h_providers
+(
+    id           bigint default nextval('h2h_providers_seq'::regclass)
+        constraint h2h_providers_pk
+            primary key,
+    provider   varchar      not null,
+    status int not null,
+	is_deleted   bool,
+    created_by   bigint,
+    created_date timestamp,
+    updated_by   bigint,
+    updated_date timestamp
+);
+
+create table h2h_provider_fees
+(
+    id           bigint default nextval('h2h_provider_fees_seq'::regclass)
+        constraint h2h_provider_fees_pk
+            primary key,
+    h2h_provider_bigint   bigint      not null,
+    wallet_code varchar(10) not null,
+	fee decimal(19,2) not null,
+	status int not null,
+	is_deleted   bool,
+    created_by   bigint,
+    created_date timestamp,
+    updated_by   bigint,
+    updated_date timestamp
+);
