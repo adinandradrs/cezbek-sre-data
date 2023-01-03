41b8482700b7653c7c00c1f8eda86d944c048627
Adinandra Dharmasurya <adinandra_dharmasurya@telkomsel.co.id>
2023-01-03 15:52:58 +0700

    Remove cashback columns from transaction to be splitted

---
+++ migration/sql/V1.017__alter_column_transaction_remove_cashbacks.sql
@@ -0,0 +1,15 @@
+alter table transactions
+    drop column reward;
+
+alter table transactions
+    drop column cashback_percentage;
+
+alter table transactions
+    drop column h2h_provider_id;
+
+alter table transactions
+    drop column h2h_provider;
+
+alter table transactions
+    drop column h2h_fee;
+
